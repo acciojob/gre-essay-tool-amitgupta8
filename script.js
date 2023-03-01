d8c7cf557c01605748a14f7f869727f9e7a79c93
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-01 12:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const textElement = document.querySelector("#evaluatedText");
 const h3Element = document.querySelector("#wordCount");
 
 function handleInput(e) {
-	const sentence = e.target.value;
-	const count = sentence.split(" ").length;
+	const match =sentence.match(/\S+/ig);
+	const count = math.length ? match.length : 0;
 	h3Element.textContent = count;
 }
 textElement.addEventListener('input', handleInput)
